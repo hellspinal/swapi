8c6ba9a46928bc926c1e3f68124e1dc6314b0ea0
hellspinal <Mackenzie.Redmon@outlook.com>
2019-12-27 22:38:12 -0600

    This was my refresher

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ function dothething(num1, num2) {
   return num1+num2;
 }
 
+function doTheOtherThing(num1, num2) {
+  return num1*num2;
+}
 
-console.log();
-#test
+const stuff=doTheOtherThing(5,9);
+const data=dothething(5,9)
+
+console.log(stuff);
+
+
