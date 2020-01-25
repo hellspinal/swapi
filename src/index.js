be8725d2796e9817812a760d102236fd4cccd5a2
hellspinal <Mackenzie.Redmon@outlook.com>
2020-01-25 00:03:14 -0600

    branching logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const math = require("math");
+
 const names=["Jeremy","David","Josh","Xerxes"];
 
 var theBoss;
@@ -7,7 +9,25 @@ function assignName(value){
 
 /////////////////////////////////////////////////////////
 
-for(let x=0;x<names.length;x++){
-  assignName(names[x])
-  console.log(theBoss)
+for(let x=100000;x>=0;x--){
+  const bru=(math.ceil(x/1000))
+  console.log(bru)
+  if (bru>80) {
+    
+  }
+  else if ( bru<=80 && bru>60 ) {
+    console.log("flag")
+  }
+  else if ( bru<=60 && bru>40 ) {
+    console.log("light")
+  }
+  else if ( bru<=40 && bru>20) {
+    console.log("boss yelling")
+  }
+  else if ( bru<=20 && bru>0 ) {
+    console.log("Siren")
+  }
+  else {
+    console.log("Machine Failure")
+  }
 }
