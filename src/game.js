dd02492d3f10c23662bd9ee79382742e74c6d762
hellspinal <mackenzie.redmon@outlook.com>
2020-02-23 21:33:59 -0600

    New changes, rewrote loop, changed if and else statements

--- src/game.js
+++ src/game.js
@@ -1,12 +1,19 @@
 const math = require("math");
 const target = math.round(math.random()*9);
 const userNumber = 5
-while(userNumber!==target){
-  const numbers = [0,1,2,3,4,5,6,7,8,9];
+if (userNumber!==target){
+  
   if(userNumber > target){
-    console.log("you have failed, fallen for the classic blunder. Also, you're answer is greater than the chosen number.")
+    console.log("You have failed, fallen for the classic blunder. Also, your answer is greater than the chosen number.")
   }
-
+  if(userNumber < target){
+    console.log("You have failed, peon, try again for a lesser number.")
+  }
+  console.log(target)
   console.log(userNumber)
 }
-
+else{
+console.log("You have won, you fool, nothing comes from guessing the right number.")
+console.log(target)
+console.log(userNumber)
+}
