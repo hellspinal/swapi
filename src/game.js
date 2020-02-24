d4adb9b70677f9a8504655a95c1077dfabaca50a
hellspinal <mackenzie.redmon@outlook.com>
2020-02-23 23:46:40 -0600

    Added error message, added user-input.

--- src/game.js
+++ src/game.js
@@ -1,19 +1,22 @@
 const math = require("math");
+const input = require("prompt-sync")();
 const target = math.round(math.random()*9);
-const userNumber = 5
+const userNumber = Number(input("Please choose a number, mortal.").trim());
+console.log(target)
+console.log(userNumber)
 if (userNumber!==target){
   
   if(userNumber > target){
     console.log("You have failed, fallen for the classic blunder. Also, your answer is greater than the chosen number.")
   }
-  if(userNumber < target){
-    console.log("You have failed, peon, try again for a lesser number.")
+  else if(userNumber < target){
+    console.log("You have failed, peon, try again for a greater number.")
   }
-  console.log(target)
-  console.log(userNumber)
+  else{
+    console.log("Inconceivable!")
+  }
 }
 else{
-console.log("You have won, you fool, nothing comes from guessing the right number.")
-console.log(target)
-console.log(userNumber)
+  console.log("You have won, you fool, nothing comes from guessing the right number.")
+
 }
