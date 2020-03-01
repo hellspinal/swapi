3cf277cb54f7f4bae0f30bf198cd5d8b310008ce
hellspinal <Mackenzie.Redmon@outlook.com>
2020-03-01 00:36:37 -0600

    Game is finished, logic and loop set

--- src/game.js
+++ src/game.js
@@ -1,22 +1,26 @@
 const math = require("math");
 const input = require("prompt-sync")();
-const target = math.round(math.random()*9);
-const userNumber = Number(input("Please choose a number, mortal.").trim());
-console.log(target)
-console.log(userNumber)
-if (userNumber!==target){
-  
-  if(userNumber > target){
+const target = math.round(math.random() * 9);
+let userNumber;
+
+
+while (userNumber !== target) {
+  console.log(userNumber)
+  userNumber = Number(input("Please choose a number, mortal.").trim());
+  if (userNumber === target) {
+    console.log("You have won, you fool, nothing comes from guessing the right number.")
+  }
+  else if (userNumber > target) {
     console.log("You have failed, fallen for the classic blunder. Also, your answer is greater than the chosen number.")
   }
-  else if(userNumber < target){
+  else if (userNumber < target) {
     console.log("You have failed, peon, try again for a greater number.")
   }
-  else{
+  else {
     console.log("Inconceivable!")
   }
 }
-else{
-  console.log("You have won, you fool, nothing comes from guessing the right number.")
 
-}
+console.log("Game over, you win")
+
+
